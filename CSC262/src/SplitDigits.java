f756609e2d3470c596f83f55a4b489937fbf5c5f
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-08 13:03:02 -0800

    program to print number without commas

--- CSC262/src/SplitDigits.java
+++ CSC262/src/SplitDigits.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class SplitDigits {
 
 	public static void main(String[] args) {
-		System.out.println("Split Digits");
-
+		Scanner in = new Scanner(System.in); // user input
+		
+		// user inputs for number between 1,000 and 999,999 
+		System.out.print("Please enter number between 1,000 and 999,999: ");
+		String number = in.next();
+		int numberLength = number.length();
+		
+		if (numberLength == 5)
+			System.out.println(number.substring(0, 1) + number.substring(2));
+		else if(numberLength == 6)
+			System.out.println(number.substring(0, 2) + number.substring(3));
+		else if(numberLength == 7)
+			System.out.println(number.substring(0, 3) + number.substring(4));
+		else
+			System.out.println("Error: number is not withing parameters (please include comma or make sure number is within range)");
 	}
 
 }
