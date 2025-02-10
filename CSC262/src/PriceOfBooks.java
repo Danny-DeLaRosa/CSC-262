41e32a085b24523cf9e22e57bcece501eac17af9
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-09 19:54:53 -0800

    price output wip

--- CSC262/src/PriceOfBooks.java
+++ CSC262/src/PriceOfBooks.java
@@ -6,7 +6,7 @@ public class PriceOfBooks {
 		// Price if book, sales tax, and shipping cost wont change (in this scenario)
 		final double bookPrice = 7.25;
 		final double salesTax = 0.085;
-		final double shippingCost = 3.5;
+		final double shippingCost = 3.50;
 		
 		// user input for many books were purchased
 		System.out.print("How many books did you purchase? ");
@@ -16,9 +16,14 @@ public class PriceOfBooks {
 		double totalBeforeTaxShipping = books * bookPrice;
 		double totalTax = (totalBeforeTaxShipping * salesTax);
 		double totalShipping = (books * shippingCost);
-		double totalAfterTaxShipping = totalTax + totalShipping;
+		double totalAfterTaxShipping = totalBeforeTaxShipping + totalTax + totalShipping;
 		
-		System.out.println("Your purchase of " + books + "books");
+		// Price breakdown of order
+		System.out.println("Your total for " + books + " books is: ");
+		System.out.printf("Total before tax and shipping: $%.2f%n" , totalBeforeTaxShipping , "Hi");
+		System.out.printf("+ Taxes: $ %.2f%n" ,totalTax);
+		System.out.printf("+ Shipping: $%.2f%n" ,totalShipping);
+		System.out.printf("Total after tax and shipping: $%.2f%n" ,totalAfterTaxShipping);
 	}
 
 }
