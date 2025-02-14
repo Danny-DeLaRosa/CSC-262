7365f8e97956ce6f29a378af92f0cad8ce4e627d
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-13 18:27:46 -0800

    supermarket coupon complete

--- CSC262/src/SupermarketCoupon.java
+++ CSC262/src/SupermarketCoupon.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
 
 public class SupermarketCoupon {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	
 		// use a loop
-		System.out.println("Supermarket coupon");
-
+		System.out.print("Please enter price of groceries: $");
+		double price = in.nextDouble();
+		// conditional statements for coupon price
+			if (price >= 210)
+			{
+				price *= 0.14;
+				System.out.printf("You are eligible for a dicount coupon of: $%.2f", price);
+			}
+			else if(price >= 150)
+			{
+				price *= 0.12;
+				System.out.printf("You are eligible for a dicount coupon of: $%.2f", price);
+			}
+			else if(price >= 60)
+			{
+				price *= 0.12;
+				System.out.printf("You are eligible for a dicount coupon of: $%.2f", price);
+			}
+			else if(price >= 10)
+			{
+				price *= 0.12;
+				System.out.printf("You are eligible for a dicount coupon of: $%.2f", price);
+			}
+			else
+			{
+				System.out.println("Youre not eligle for a coupon broke boy");
+			}		
 	}
 
 }
