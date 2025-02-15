4ae670a717c0773de0875aa166d36c4d2b0115e3
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-15 12:49:13 -0800

    sum of odds app complete

--- CSC262/src/SumOfOdds.java
+++ CSC262/src/SumOfOdds.java
@@ -1,9 +1,24 @@
-
+import java.util.Scanner;
 public class SumOfOdds {
 
 	public static void main(String[] args) {
-		System.out.println("Sum of odd digits");
-
+		Scanner in = new Scanner(System.in);
+				
+		System.out.println("Input integer: ");
+		int input = in.nextInt();
+		
+		int sumOfOdds = 0;
+		while(input > 0)
+		{
+			int lastDigit = input % 10;
+			if(lastDigit % 2 == 1)
+			{
+				sumOfOdds += lastDigit;
+			}
+			input = input / 10;
+			
+		}
+		System.out.println(sumOfOdds);
 	}
 
 }
