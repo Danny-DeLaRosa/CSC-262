2650c7ff6479e0396f2e24f4a845f0eb0ed85bda
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-21 12:22:46 -0800

    stuck, wip

--- CSC262/src/Swapping.java
+++ CSC262/src/Swapping.java
@@ -2,8 +2,17 @@
 public class Swapping {
 
 	public static void main(String[] args) {
-		System.out.println("Swapping");
+		// read a word
+		String word = "Programming";
+		double rand = Math.random() * (word.length() - 2);
+		rand = Math.ceil(rand);
+		System.out.println(rand);
 
+	}
+	public static String swapLetter(String a) {
+		System.out.println(a);
+		int rand = (int)Math.ceil(Math.random()) * a.length() - 1;
+		return a;
 	}
 
 }
