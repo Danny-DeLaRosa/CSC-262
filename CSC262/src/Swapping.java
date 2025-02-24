fda14f93c69491498f9906368717c750e83cd689
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-24 15:18:41 -0800

    swapping complete

--- CSC262/src/Swapping.java
+++ CSC262/src/Swapping.java
@@ -36,20 +36,9 @@ public class Swapping {
 
 	        System.out.println("Permuted word: " + permuted.toString());
 	        in.close();
-		
-	}
-	}
+	
 		// read a word
-		String word = "Programming";
-		double rand = Math.random() * (word.length() - 2);
-		rand = Math.ceil(rand);
-		System.out.println(rand);
 
 	}
-	public static String swapLetter(String a) {
-		System.out.println(a);
-		int rand = (int)Math.ceil(Math.random()) * a.length() - 1;
-		return a;
-	}
-
 }
+
