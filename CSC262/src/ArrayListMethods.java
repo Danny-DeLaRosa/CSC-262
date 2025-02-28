83dd36e0140c99ab58452f290a702bcece12ed6f
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-28 14:40:15 -0800

    Moved uppercase strings to front of array

--- CSC262/src/ArrayListMethods.java
+++ CSC262/src/ArrayListMethods.java
@@ -7,21 +7,38 @@ public class ArrayListMethods {
 		ArrayList<String> words = new ArrayList<>(List.of(
 			    "Apple", "banana", "Cherry", "dog", "Elephant", "fox", "Grapes", "hippo", "Ice", "jaguar"
 			));
-		print_array_list(words);
+		System.out.print("Original: "); print_array_list(words);
+		System.out.print("\nModified: "); move_uppercase_front(words);
 	}
 	
 	public static void print_array_list(ArrayList<String> a) {
-		if(a.size() > 1) {
-			for(String e : a) 
-				if(e != a.get(a.size() - 1)){
-				System.out.print(e + ", ");
-				}
-				else {
-					System.out.print(e);
-			}
-			System.out.println();
-			
+		  for (int i = 0; i < a.size(); i++) {
+		        System.out.print(a.get(i));
+		        if (i < a.size() - 1) {
+		            System.out.print(", ");
+		        }
+		    }
+		    System.out.println();
 		}
+	
+	public static void move_uppercase_front(ArrayList<String> a) {
+		ArrayList<String> uppercase = new ArrayList<>();
+		ArrayList<String> lowercase = new ArrayList<>();
+		
+		for(int i = 0; i < a.size(); i++) {
+			String str = a.get(i);
+			Character firstChar = str.charAt(0);
+			if(Character.isUpperCase(firstChar)) {
+				uppercase.add(str);
+			}
+			else {
+				lowercase.add(str);
+			}
+		}
+		a.clear();
+		a.addAll(uppercase);
+		a.addAll(lowercase);
+		print_array_list(a);
 	}
 
 }
