b47409ab271cc96aed580f0f64e510de897b6494
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-28 11:08:04 -0800

    swapping

--- CSC262/src/ArrayMethods.java
+++ CSC262/src/ArrayMethods.java
@@ -2,8 +2,18 @@
 public class ArrayMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] values = {1, 82, 94, 4, 36, 7, 55};
+		swap(values);
+	}
+	public static void swap(int[] a) {
+		int first = a[0];
+		int last = a[a.length - 1];
+		a[0] = last;
+		a[a.length - 1] = first;
+		for(int i = 0; i < a.length; i++) {
+			if(a.length > 1)
+			System.out.print(" " + a[i]);
+		}
 	}
 
 }
