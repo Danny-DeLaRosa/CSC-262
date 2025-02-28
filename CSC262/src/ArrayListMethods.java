ed94cbabc51947dbe0dadeafa309f965a6fc03a2
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-28 13:39:20 -0800

    print array list

--- CSC262/src/ArrayListMethods.java
+++ CSC262/src/ArrayListMethods.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> words = new ArrayList<>(List.of(
+			    "Apple", "banana", "Cherry", "dog", "Elephant", "fox", "Grapes", "hippo", "Ice", "jaguar"
+			));
+		print_array_list(words);
+	}
+	
+	public static void print_array_list(ArrayList<String> a) {
+		if(a.size() > 1) {
+			for(String e : a) 
+				if(e != a.get(a.size() - 1)){
+				System.out.print(e + ", ");
+				}
+				else {
+					System.out.print(e);
+			}
+			System.out.println();
+			
+		}
 	}
 
 }
